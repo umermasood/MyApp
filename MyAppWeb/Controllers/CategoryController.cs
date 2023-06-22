51295b58e83cfefb4d88d3223880c8d144d42d7f
Umer Masood <umermasood.dev@gmail.com>
2023-06-22 10:12:28 +0500

    03 - Server Side Validation

--- MyAppWeb/Controllers/CategoryController.cs
+++ MyAppWeb/Controllers/CategoryController.cs
@@ -28,9 +28,13 @@ namespace MyAppWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category category)
         {
-            _context.Categories.Add(category);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
         }
     }
 }
